2126798462aec101e5f222a433f437f15f2de668
wanko <kouji124@w4.dion.ne.jp>
2021-04-11 16:13:22 -0700

    created simple base

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import axios from "axios";
-import { Button } from "react-materialize";
+import { Button, Row, Textarea } from "react-materialize";
 
 const Dashboard = () => {
   const [firstName, setFirstName] = useState();
 
   const logout = () => {
-      axios.post('/logout').then((response) => {
-          console.log(response);
-      })
-      localStorage.clear()
-
-  }
+    axios.post("/logout").then((response) => {
+      console.log(response);
+    });
+    localStorage.clear();
+  };
 
   useEffect(() => {
     axios.get("/login").then((response) => {
@@ -25,9 +24,10 @@ const Dashboard = () => {
         localStorage.setItem("lastName: ", response.data.lastName);
         localStorage.setItem("email: ", response.data.email);
         localStorage.setItem("token", response.data.token);
-        setFirstName(localStorage.getItem("firstName"));
+        // setFirstName(localStorage.getItem("firstName"));
       }
     });
+    setFirstName(localStorage.getItem("firstName"));
   }, []);
 
   return (
@@ -35,10 +35,20 @@ const Dashboard = () => {
       <h2>Welcome back, {firstName}!</h2>
       <br />
       <br />
+      <Row>
+        <Textarea id="Textarea-12" l={12} m={12} s={12} xl={12} />
+      </Row>
+      <br />
+      <br />
       <Link to="/signin">
-        <Button node="button" style={{ marginRight: "5px" }} waves="light" onClick={logout}>
-        Logout
-      </Button>
+        <Button
+          node="button"
+          style={{ marginRight: "5px" }}
+          waves="light"
+          onClick={logout}
+        >
+          Logout
+        </Button>
       </Link>
     </div>
   );
